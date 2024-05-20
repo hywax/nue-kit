f8f00397cfb88b9c821198dcb8d108be640a3fce
hywax <me@hywax.space>
2024-05-20 21:38:45 +0500

    chore: provide type store

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,8 @@
-import type { StoreScheme } from './core/store'
+import type { Store, StoreScheme } from './core/store'
 import type { Locale, LocaleDictionary, LocaleMessages } from './core/i18n'
 
 export {
+  Store,
   StoreScheme,
   LocaleDictionary,
   Locale,
